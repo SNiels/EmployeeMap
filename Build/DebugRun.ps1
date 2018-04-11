dc3bbe43950e28fdd4bd7d575dd338dd41bc9be7
Niels Swimberghe <nielsswimberghe@gmail.com>
2018-04-11 17:48:17 -0400

    Add detach script

--- Build/DebugRun.ps1
+++ Build/DebugRun.ps1
@@ -1,5 +1,7 @@
 docker-compose -f "./docker-compose.yml" -f "./docker-compose.override.yml" -f "./docker-compose.vs.debug.g.yml" --no-ansi up -d
 
+Start-Sleep -Seconds 5
+
 $appId = ((docker ps --filter "ancestor=employeemapapp:debug")[1] -split " ")[0]
 $apiId = ((docker ps --filter "ancestor=employeemapapi:debug")[1] -split " ")[0]
 
@@ -12,5 +14,14 @@ docker exec -d $apiId C:\\remote_debugger\\x64\\msvsmon.exe /noauth /anyuser /si
 $appTarget = $appId + ":4022"
 $apiTarget = $apiId + ":4022"
 
-./RemoteDebugAttach.exe 15 "Remote (no authentication)" $appTarget "dotnet.exe"
-./RemoteDebugAttach.exe 15 "Remote (no authentication)" $apiTarget "dotnet.exe"
+./RemoteDebugAttach.exe "EmployeeMap.sln" "Remote (no authentication)" $appTarget "dotnet.exe"
+./RemoteDebugAttach.exe "EmployeeMap.sln" "Remote (no authentication)" $apiTarget "dotnet.exe"
+
+Read-Host "Press any key to quit"
+
+./RemoteDebugDetach.exe EmployeeMap.sln "dotnet.exe"
+
+docker exec -d $appId C:\\remote_debugger\\x64\\utils\\KillProcess.exe msvsmon.exe
+docker exec -d $appId C:\\remote_debugger\\x64\\utils\\KillProcess.exe dotnet.exe
+docker exec -d $apiId C:\\remote_debugger\\x64\\utils\\KillProcess.exe msvsmon.exe
+docker exec -d $apiId C:\\remote_debugger\\x64\\utils\\KillProcess.exe dotnet.exe
